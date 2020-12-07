7380d199470a334524b80ee35297ff6b06cf77c3
CYQAQ555 <1019115527@qq.com>
2020-12-07 21:41:41 +0800

    练习

--- 2020 12 6/src/IntegerBit.java
+++ 2020 12 6/src/IntegerBit.java
@@ -1,6 +1,19 @@
 import java.util.Scanner;
 public class IntegerBit {
+    public static void printf(int n) {
+        if(n > 9){
+            printf(n/10);
+        }
+        System.out.println(n%10);
+    }
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("请输入一个数：");
+        int n = sc.nextInt();
+        System.out.println("其每一位是：");
+        printf(n);
+    }
+    public static void main1(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.println("请输入一个数：");
         int n = sc.nextInt();

8ed06e9842c69336a91b448a9f7840178abf9c1c
CYQAQ555 <1019115527@qq.com>
2021-02-01 18:08:04 +0800

    Test

---
+++ 2021 1 29/src/Test.java
@@ -0,0 +1,25 @@
+
+    class Animal{
+        public void move(){
+            System.out.println("动物可以移动");
+        }
+    }
+    class Dog extends Animal{
+        public void move(){
+            System.out.println("狗可以跑和走");
+        }
+        public void bark(){
+            System.out.println("狗可以吠叫");
+        }
+    }
+    public class Test {
+        public static void main(String args[]){
+            Animal a = new Animal();
+            Dog b = new Dog();
+            //Animal b = new Dog();编译看左边，运行看右边。 父类型引用指向子类型对象，无法调用只在子类型里定义的方法,编译错误
+            a.move();
+            b.move();
+            b.bark();
+        }
+    }
+
